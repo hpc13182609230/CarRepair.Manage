f3ba2518b48cf81236f1c52dd2f47ae4c38aad7d
hpc13182609230 <1224579114@qq.com>
2017-07-31 22:15:19 +0800

    Wechat_Login

---
+++ CarRepairManage/CarRepairAPI/Controllers/LoginController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ViewModel;
+
+namespace CarRepairAPI.Controllers
+{
+    [RoutePrefix("api/Login")]
+    public class LoginController : ApiController
+    {
+        [Route("Wechat_Login")]
+        [HttpGet]
+        public DataResultModel Wechat_Login (string thirdSession ="",string code="")
+        {
+            DataResultModel result = new DataResultModel();
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                result.rs = 0;
+                result.em = ex.Message;
+            }
+            return result;
+        }
+
+    }
+}

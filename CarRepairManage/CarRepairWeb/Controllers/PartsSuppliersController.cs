cda21fcc747e42e02f2f5818386612481f2a4752
hpc13182609230 <1224579114@qq.com>
2017-08-14 17:26:51 +0800

    后台 内容 确定

---
+++ CarRepairManage/CarRepairWeb/Controllers/PartsSuppliersController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CarRepairWeb.Controllers
+{
+    public class PartsSuppliersController : Controller
+    {
+        // GET: PartsSuppliers
+        public ActionResult PartsList()
+        {
+            return View();
+        }
+    }
+}
